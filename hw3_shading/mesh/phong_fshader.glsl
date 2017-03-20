7ce0cd7327e9503b03babd22cda4d4ec4ae22851
Stephane Selim <stephan.selim@epfl.ch>
2017-03-20 17:43:51 +0100

    Update phong_fshader.glsl

--- hw3_shading/mesh/phong_fshader.glsl
+++ hw3_shading/mesh/phong_fshader.glsl
@@ -1,5 +1,11 @@
 #version 330
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
+uniform float alpha;
 
+in vec3 light_dir;
+in vec3 view_dir;
+in vec3 normal_mv;
 
 out vec3 color;
 
@@ -7,7 +13,7 @@ out vec3 color;
 void main() {
     color = vec3(0.0,0.0,0.0);
 
-    const vec3 COLORS[6] = vec3[](
+    /**const vec3 COLORS[6] = vec3[](
         vec3(1.0,0.0,0.0),
         vec3(0.0,1.0,0.0),
         vec3(0.0,0.0,1.0),
@@ -15,7 +21,7 @@ void main() {
         vec3(0.0,1.0,1.0),
         vec3(1.0,0.0,1.0));
     int index = int( mod(gl_PrimitiveID,6) );
-    color = COLORS[index];
+    color = COLORS[index];*/
 
     //>>>>>>>>>> TODO >>>>>>>>>>>
     // TODO 1.2: Phong shading.
@@ -26,4 +32,17 @@ void main() {
     // the code above after
     // implementing Phong shading.
     //<<<<<<<<<< TODO <<<<<<<<<<<
+    vec3 ambient = La * ka;
+    vec3 n = normalize(normal_mv);
+    vec3 l = normalize(light_dir);
+    float lambert = dot(n,l);
+    vec3 diffuse = vec3(0.0, 0.0, 0.0);
+    vec3 specular = vec3(0.0, 0.0, 0.0);
+    if(lambert > 0.0) {
+        diffuse = Ld*kd*lambert;
+        vec3 v = normalize(view_dir);
+        vec3 r = reflect(-l,n);
+        specular = Ls*ks*pow(max(dot(r,v), 0.0), alpha);
+    }
+    color += ambient + diffuse + specular;
 }
