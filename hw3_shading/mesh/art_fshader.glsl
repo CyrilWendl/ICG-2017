be912b6c1b202879203f2b6265a24722140b150a
Stephane Selim <stephan.selim@epfl.ch>
2017-03-21 16:38:54 +0100

    Update art_fshader.glsl

--- hw3_shading/mesh/art_fshader.glsl
+++ hw3_shading/mesh/art_fshader.glsl
@@ -6,9 +6,6 @@ in vec3 normal_mv;
 
 out vec3 color;
 
-uniform vec3 La, Ld, Ls;
-uniform vec3 ka, kd, ks;
-
 uniform float alpha;
 uniform sampler2D tex2D;
 
@@ -30,22 +27,17 @@ void main() {
     // 1) compute the output color by doing a look-up in the texture using the
     //    texture sampler tex.
     //<<<<<<<<<< TODO <<<<<<<<<<<
-    //vec3 ambient = La * ka;
     vec3 n = normalize(normal_mv);
     vec3 l = normalize(light_dir);
     float lambert = dot(n,l);
-    //vec3 diffuse = vec3(0.0, 0.0, 0.0);
-    //vec3 specular = vec3(0.0, 0.0, 0.0);
     vec4 text_value = vec4(0.0, 0.0, 0.0, 0.0);
     if(lambert > 0.0) {
-        //diffuse = Ld*kd*lambert;
         vec3 v = normalize(view_dir);
         vec3 r = reflect(-l,n);
         vec2 tex_coord = vec2(lambert, pow(max(dot(r,v), 0.0), alpha));
         text_value = texture(tex2D, tex_coord);
-        //specular = Ls*ks*text_value.r;
     }
-    //color += ambient + diffuse + specular;
-    color += text_value.rgb ;
+
+    color += text_value.rgb;
 }
 
