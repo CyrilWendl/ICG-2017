881d04a44a199cc6677906fea5795c6b7646f718
Cyril Wendl <cyrilwendl@gmail.com>
2017-03-22 22:12:40 +0100

    Spot shaders added

--- build/hw3_shading/mesh_fshader.glsl
+++ build/hw3_shading/mesh_fshader.glsl
@@ -3,9 +3,9 @@
 
 out vec3 color;
 
-
-const float spot_cos_cutoff = 0.985; // cos 10 degrees
-const float spot_exp = 150;
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
+uniform float alpha;
 
 void main() {    
     const vec3 COLORS[6] = vec3[](
@@ -17,9 +17,4 @@ void main() {
         vec3(1.0,0.0,1.0));
     int index = int( mod(gl_PrimitiveID,6) );
     color = COLORS[index];
-
-    //>>>>>>>>>> TODO >>>>>>>>>>>
-    // TODO 5: Spot light.
-    // Complete the shader to obtain spot light effect
-    //<<<<<<<<<< TODO <<<<<<<<<<<
 }
--- hw3_shading/mesh/spot_vshader.glsl
+++ hw3_shading/mesh/spot_vshader.glsl
@@ -1,17 +1,22 @@
 #version 330
-
-in vec3 vpoint;
-in vec3 vnormal;
-
-
 uniform mat4 projection;
 uniform mat4 model;
 uniform mat4 view;
 uniform vec3 light_pos;
-uniform vec3 spot_dir;
+
+in vec3 vpoint;
+in vec3 vnormal;
+
+out vec3 light_dir;
+out vec3 view_dir;
+out vec3 normal_mv;
+
 
 void main() {
     mat4 MV = view * model;
     vec4 vpoint_mv = MV * vec4(vpoint, 1.0);
     gl_Position = projection * vpoint_mv;
+    normal_mv = normalize(mat3(transpose(inverse(MV))) * vnormal);
+    light_dir = light_pos - vpoint_mv.xyz;
+    view_dir = -vpoint_mv.xyz;
 }
