826e33d2c38f1451d3993fbfa85c3c7b1364fad5
Stephane Selim <stephan.selim@epfl.ch>
2017-03-28 12:16:31 +0200

    Update screenquad_fshader.glsl

--- hw4p3_motionblur/screenquad/screenquad_fshader.glsl
+++ hw4p3_motionblur/screenquad/screenquad_fshader.glsl
@@ -9,5 +9,13 @@ void main() {
     /// TODO: use a constant number of samples for integral (what happens if you take too few?)
     /// HINT: you can scale the velocity vector to make the motion blur effect more prominent
     /// HINT: to debug integration don't use the velocityTex, simply assume velocity is constant, e.g. vec2(1,0)
-    color = texture(colorTex, uv);
+    //color = texture(colorTex, uv);
+    vec2 velocity = texture(velocityTex, uv).xy;
+    int N = 30;
+    vec4 color_total = vec4(0.0, 0.0, 0.0, 0.0);
+    for(int i = 0; i < N; i++) {
+        //proper indexing i/N since we need values from 0 to 1
+        color_total += texture(colorTex, ((uv + velocity*(float(i)/float(N)))));
+    }
+    color = color_total / float(N);
 }
