2ce040783cfa8320d7559040e5e8c1301a0f64b8
Cyril Wendl <cyrilwendl@gmail.com>
2017-04-26 18:04:26 +0200

    Added new texture and snow on top.

--- icg_project/grid/grid_vshader.glsl
+++ icg_project/grid/grid_vshader.glsl
@@ -3,6 +3,7 @@
 in vec2 position;
 
 out float height;
+out float scaling_height_factor;
 out vec2 uv;
 
 //in vec3 vpoint;
@@ -13,7 +14,6 @@ uniform float time;
 uniform sampler2D texNoise;      // pass the texture also in the vertex shader to compute the height
 
 in vec3 color;
-
 in vec2 vtexcoord;
 
 void main() {
@@ -21,7 +21,7 @@ void main() {
 
     // convert the 2D position into 3D positions that all lay in a horizontal
     // plane.
-    float scaling_height_factor = 2.0;
+    scaling_height_factor = 2.0;
     height = float(texture(texNoise,uv).x) / scaling_height_factor;        // divide by a scaling factor
     vec3 pos_3d = vec3(uv.x, uv.y, -height);        // or position instead of uv
 
