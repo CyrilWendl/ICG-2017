a739c1604981dfc507168ff55ef2886a72fe4932
Stephane Selim <stephan.selim@epfl.ch>
2017-05-02 00:34:10 +0200

    Not draw skybox for now

--- icg_project/main.cpp
+++ icg_project/main.cpp
@@ -177,7 +177,7 @@ void Display() {
     framebuffer.Unbind();
     glViewport(0,0,window_width,window_height);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    skybox.Draw(projection_matrix * view_matrix * trackball_matrix * quad_model_matrix);
+    //skybox.Draw(projection_matrix * view_matrix * trackball_matrix * quad_model_matrix);
     grid.Draw(time, trackball_matrix * quad_model_matrix, view_matrix, projection_matrix);
     water.Draw(time, trackball_matrix * quad_model_matrix, view_matrix, projection_matrix);
 
